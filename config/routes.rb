0d465eb5dfb5e33c140d10b7c7967f8da4124fb2
MateuszADV <mateusz.adv@gmail.com>
2021-10-28 18:08:18 +0200

    add ne message

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   resources :rooms do
     resource :messages
   end
-
+  root to: 'rooms#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
